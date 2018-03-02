43a9326a90b5ca15b4f491ecb319013ed5b61638
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-02 11:00:18 -0300

    Desfazendo merge do repositório

--- eva/eva/settings.py
+++ eva/eva/settings.py
@@ -15,7 +15,7 @@ import os
 from wit import Wit
 
 # WIT API - Constructor - TOKEN
-eva_ia = Wit("WIT-API-TOKEN")
+eva_ia = Wit("WIT-TOKEN")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

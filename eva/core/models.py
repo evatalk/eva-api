b4be200838452f8a0696a6440b8310b40e6e5f91
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-04 23:11:31 -0300

    Criando models iniciais

--- eva/core/models.py
+++ eva/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class MessagingService(models.Models):
+    name = models.CharField(max_length=100)
+
+
+class User(models.Models):
+    first_name = models.CharField(max_length=100)
+
+
+class Message(models.Models):
+    content = models.CharField(max_length=255)
+    intent = models.CharField(max_length=50)
+    received_at = models.DateField(auto_now_add=True)
+
+
+class MessagingServiceUserIdentifier(models.Models):
+    messaging_service = models.ForeignKey(
+        User, related_name="messaging_service_user_identifier")
+    user = models.ForeignKey(
+        User, related_name="messaging_service_user_identifier")
+
+
+class UserMessage(models.Models):
+    message = models.ForeignKey(Message, related_name="user_messages")
+    user = models.ForeignKey(User, related_name="user_messages")

b1bc5d22049679c5e4029f59c6e6ab4b15f21a21
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-05 23:11:38 -0300

    Criando BaseModel para armazenar a data de criação

--- eva/core/models.py
+++ eva/core/models.py
@@ -1,28 +1,33 @@
 from django.db import models
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class MessagingService(models.Model):
+    class Meta:
+        abstract = True
+
+class MessagingService(BaseModel):
     name = models.CharField(max_length=100)
 
 
-class User(models.Model):
+class User(BaseModel):
     first_name = models.CharField(max_length=100)
 
 
-class Message(models.Model):
+class Message(BaseModel):
     content = models.CharField(max_length=255)
     intent = models.CharField(max_length=50)
     received_at = models.DateField(auto_now_add=True)
 
 
-class MessagingServiceUserIdentifier(models.Model):
+class MessagingServiceUserIdentifier(BaseModel):
     messaging_service = models.ForeignKey(
         MessagingService, related_name="messaging_service_user_identifier", on_delete=models.CASCADE)
     user = models.ForeignKey(
         User, related_name="messaging_service_user_identifier", on_delete=models.CASCADE)
 
 
-class UserMessage(models.Model):
+class UserMessage(BaseModel):
     message = models.ForeignKey(
         Message, related_name="user_messages", on_delete=models.CASCADE)
     user = models.ForeignKey(

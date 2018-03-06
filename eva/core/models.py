91e95e07d00a8ca2546c8af02b788263a7de34c2
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-05 22:56:32 -0300

    Fix nos models e iniciando implementação de testes

--- eva/core/models.py
+++ eva/core/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 
 
-class MessagingService(models.Models):
+class MessagingService(models.Model):
     name = models.CharField(max_length=100)
 
 
-class User(models.Models):
+class User(models.Model):
     first_name = models.CharField(max_length=100)
 
 
-class Message(models.Models):
+class Message(models.Model):
     content = models.CharField(max_length=255)
     intent = models.CharField(max_length=50)
     received_at = models.DateField(auto_now_add=True)
 
 
-class MessagingServiceUserIdentifier(models.Models):
-    messaging_service_id = models.ForeignKey(
-        User, related_name="messaging_service_user_identifier")
-    user_id = models.ForeignKey(
-        User, related_name="messaging_service_user_identifier")
+class MessagingServiceUserIdentifier(models.Model):
+    messaging_service = models.ForeignKey(
+        MessagingService, related_name="messaging_service_user_identifier", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="messaging_service_user_identifier", on_delete=models.CASCADE)
 
 
-class UserMessage(models.Models):
-    message_id = models.ForeignKey(Message, related_name="user_messages")
-    user_id = models.ForeignKey(User, related_name="user_messages")
+class UserMessage(models.Model):
+    message = models.ForeignKey(
+        Message, related_name="user_messages", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="user_messages", on_delete=models.CASCADE)
--- eva/core/tests.py
+++ eva/core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
 
-# Create your tests here.
+
+class EVATests(APITestCase):
+
+    def setUp(self):
+        factory = APIRequestFactory()
+
+    def test_get(self):
+        url = reverse('request_handler')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)

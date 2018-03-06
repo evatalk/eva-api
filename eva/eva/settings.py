c425a64e5f86914af0c414be37e94f7b48082a31
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-05 23:31:14 -0300

    Refatorando hierarquia de pastas da EVA

--- eva/eva/settings.py
+++ eva/eva/settings.py
@@ -56,6 +56,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'updaters',
+    'controllers',
+    'handlers',
 ]
 
 MIDDLEWARE = [
---
+++ eva/updaters/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UpdatersConfig(AppConfig):
+    name = 'updaters'

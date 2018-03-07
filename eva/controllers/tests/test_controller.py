e789d604022472a10b4eb03e8969dc903398aadf
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-07 00:02:08 -0300

    Criando testes iniciais do controller

---
+++ eva/controllers/tests/test_controller.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+
+from core.models import MessagingService, MessagingServiceUserIdentifier, User
+
+from ..request_controller import ModelDataVerifier
+
+
+class ControllerTestCase(TestCase):
+    def setUp(self):
+        # Creating User and Messaging test models
+        User.objects.create(first_name="Mauro de Carvalho",
+                            messaging_service_identifier="iu124h21h2i21h")
+
+        User.objects.create(first_name="Leonardo Minora",
+                            messaging_service_identifier="fiuaha2h2hs211")
+
+        MessagingService.objects.create(name="Telegram")
+
+        # Getting test models
+        user = User.objects.get(
+            first_name="Mauro de Carvalho", messaging_service_identifier="iu124h21h2i21h")
+
+        messaging_service = MessagingService.objects.get(name="Telegram")
+
+        # Creating MessagingUserIdentifier test model
+        MessagingServiceUserIdentifier.objects.create(
+            user=user, messaging_service=messaging_service)
+
+    def test_verifies_if_user_exists(self):
+        self.assertEqual(ModelDataVerifier.verifies_if_user_exists(
+            "iu124h21h2i21h"), True)
+
+    def test_verifies_if_user_doesnt_exists(self):
+        self.assertEqual(ModelDataVerifier.verifies_if_user_exists(
+            "49082412902292"), False)
+
+    def test_verifies_if_messaging_service_exists(self):
+        self.assertEqual(
+            ModelDataVerifier.verifies_if_messaging_service_exists("Telegram"), True)
+
+    def test_verifies_if_messaging_service_doesnt_exists(self):
+        self.assertEqual(
+            ModelDataVerifier.verifies_if_messaging_service_exists("WhatsApp"), False)
+
+    def test_verifies_if_messaging_service_user_identifier_exists(self):
+        user = User.objects.get(messaging_service_identifier="iu124h21h2i21h")
+        messaging_service = MessagingService.objects.get(name="Telegram")
+        self.assertEqual(
+            ModelDataVerifier.verifies_if_messaging_service_identifier_exists(user, messaging_service), True)
+
+    def test_verifies_if_messaging_service_user_identifier_doesnt_exists(self):
+        user = User.objects.get(messaging_service_identifier="fiuaha2h2hs211")
+        messaging_service = MessagingService.objects.get(name="Telegram")
+        self.assertEqual(
+            ModelDataVerifier.verifies_if_messaging_service_identifier_exists(user, messaging_service), False)

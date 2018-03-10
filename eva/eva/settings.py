48853e3b146cdc5d18af454753156f5897ec34f2
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-10 20:34:04 -0300

    Adicionando DRF Token à aplicação

--- eva/eva/settings.py
+++ eva/eva/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'core',
     'controllers',
     'handlers',
--- eva/handlers/readers/csv_reader.py
+++ eva/handlers/readers/csv_reader.py
@@ -1,1 +1,30 @@
 import csv
+
+
+class CSVFileReader(object):
+
+    def __init__(self, file_name):
+        self.csv_file_name = file_name
+
+    def check_if_email_exists(self, email_address):
+        with open(self.csv_file_name, encoding='utf-16-le', newline='') as f:
+            reader = csv.reader(f.readlines(), delimiter='|', quotechar='|')
+            count = 0
+            for row in reader:
+                a = row
+                print(a)
+                print("\n")
+                print("Tamanho", len(a))
+                print("\n")
+                count += 1
+                if count == 2:
+                    break
+
+    def close(self):
+        pass
+
+
+if __name__ == "__main__":
+
+    test = CSVFileReader("csv_utf16.csv")
+    test.check_if_email_exists("cu")

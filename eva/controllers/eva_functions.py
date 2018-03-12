977b42ad99de8a878de8a768247c6bc53e5a0668
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-11 23:38:51 -0300

    Criando classe para controlar as respostas da EVA

---
+++ eva/controllers/eva_functions.py
@@ -0,0 +1,63 @@
+from random import choice
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from controllers.request_controller import UserRequestInformation
+from eva.settings import INFORMATIONS_STORAGE_PATH
+from handlers.conversations.responses import RESPONSE_MAP
+from handlers.readers.information_readers import StorageInformationReader
+
+
+class EvaController(object):
+
+    def __init__(self, intent, request):
+        self.intent = intent
+        self.request = request
+
+    def response(self):
+        return self.intent_map()
+
+    def intent_map(self):
+        if self.intent == "greetings":
+            return self.returns_a_greetings_response()
+
+        elif self.intent == "cursing":
+            return self.returns_a_non_cursing_response()
+
+        elif self.intent == "history":
+            return self.returns_the_user_courses_history()
+
+        else:
+            return self.returns_a_default_response("default")
+
+    def returns_the_user_courses_history(self):
+        data_reader = StorageInformationReader(INFORMATIONS_STORAGE_PATH)
+
+        user_cpf = UserRequestInformation.get_user_cpf(self.request)
+
+        users_courses_data = data_reader.user_courses_history(user_cpf)
+
+        status_code = status.HTTP_200_OK
+
+        return Response({"content": users_courses_data}, status=status_code)
+
+    def returns_a_greetings_response(self):
+        greetings_message = choice(RESPONSE_MAP[self.intent])
+        status_code = status.HTTP_200_OK
+
+        return Response({"message": greetings_message}, status=status_code)
+
+    def returns_a_non_cursing_response(self):
+        non_cursing_message = choice(RESPONSE_MAP[self.intent])
+        status_code = status.HTTP_200_OK
+
+        return Response({"message": non_cursing_message}, status=status_code)
+
+    def returns_a_default_response(self, intent):
+        default_message = choice(RESPONSE_MAP[intent])
+        status_code = status.HTTP_200_OK
+
+        return Response({"message": default_message}, status=status_code)

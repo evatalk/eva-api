6fd6b423be9ff58e4bc6c24586c68c32037274c6
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-14 01:35:40 -0300

    Criando função para auxiliar na concatenação de textos

---
+++ eva/handlers/writers/text_helpers.py
@@ -0,0 +1,6 @@
+class TextHandler(object):
+    @classmethod
+    def concatenate_information_to_text(cls, base_text, information_data):
+        text_to_concatenate = ", ".join(information_data)
+
+        return base_text.format(text_to_concatenate)

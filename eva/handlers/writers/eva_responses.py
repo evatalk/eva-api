7eda2f18e6b62b981870e1ae5f3fab2d1f3bdaa1
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-15 00:24:30 -0300

    Finalizando função para enviar os cursos finalizados do aluno com as instruções para a emissão de certificado

--- eva/handlers/writers/eva_responses.py
+++ eva/handlers/writers/eva_responses.py
@@ -11,24 +11,43 @@ class EvaResponseWriter(object):
     def finished_courses_response(self):
         finished_courses_data = self._analyze_data_finished_courses(
             self.user_informations)
+        
+        json_response = {}
 
         if finished_courses_data["after_2015"] is not None:
             list_of_course_names = self._get_list_of_finished_courses(
                 finished_courses_data["after_2015"])
 
-            # TextHandler.concatenate_information_to_text(,)
+            base_text = RESPONSE_MAP["finished_courses"]["after_2015"]
+
+            after_2015_response_text = TextHandler.concatenate_information_to_text(
+                base_text, list_of_course_names)
+            
+            json_response["after_2015"] = after_2015_response_text
 
         if finished_courses_data["between_2013_to_2014"] is not None:
             list_of_course_names = self._get_list_of_finished_courses(
                 finished_courses_data["between_2013_to_2014"])
 
-            # TextHandler.concatenate_information_to_text(,)
+            base_text = RESPONSE_MAP["finished_courses"]["between_2013_and_2014"]
+
+            between_2013_to_2014_response_text = TextHandler.concatenate_information_to_text(
+                base_text, list_of_course_names)
+
+            json_response["between_2013_to_2014"] = between_2013_to_2014_response_text
 
         if finished_courses_data["before_2013"] is not None:
             list_of_course_names = self._get_list_of_finished_courses(
                 finished_courses_data["before_2013"])
 
-            # TextHandler.concatenate_information_to_text(,)
+            base_text = RESPONSE_MAP["finished_courses"]["before_2013"]
+
+            before_2013_response_text = TextHandler.concatenate_information_to_text(
+                base_text, list_of_course_names)
+            
+            json_response["between_2013_to_2014"] = before_2013_response_text
+        
+        return json_response
 
     def _analyze_data_finished_courses(self, user_informations):
         information_analyzer = FinishedCoursesInformationAnalyzer(

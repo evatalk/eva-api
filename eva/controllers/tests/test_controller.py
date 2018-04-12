18bd591f383885e91f51a1b7fd7245b8c68b14d4
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-04-11 22:10:47 -0300

    Retirando módulos de testes em pacotes diferentes

--- eva/controllers/tests/test_controller.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-from core.models import MessagingService, MessagingServiceUserIdentifier, User
-
-from ..request_controller import ModelDataVerifier, RequestDataVerifier
